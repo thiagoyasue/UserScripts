191f0a6f2c104eb34cc20c015ed3d8fd59646879
Thiago Yasue <thiagoyukio1@hotmail.com>
2023-09-21 00:07:27 -0300

    Delete contaBatata.js

--- contaBatata.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name         Contador de batata muito legal
-// @description  Conta as batata do grundos.cafe procê (aprecie com moderação)
-// @author       thiagoyasue
-// @include        http*://www.grundos.cafe/medieval/potatocounter/
-// ==/UserScript==
-
-var src = document.querySelectorAll("img[src*='/games/potato']");
-$("input[name='potatoes']").val(src.length-1);
